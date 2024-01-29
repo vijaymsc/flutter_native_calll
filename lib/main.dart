dee3fe7960988a2433ac02ad2f5e64ff0aa353e3
vijaymsc <vijayvsjc15@gmail.com>
2024-01-29 18:29:16 +0530

    commit via android studio

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: (){
              fetchDataFromNative();
               },
-             child: const Text("click"),
+             child: const  Text("click"),
            )
           ],
         ),
